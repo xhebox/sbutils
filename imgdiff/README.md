cd5f23e9d38e279c6486ed6b29d51ea8560c9756
xhe <xw897002528@gmail.com>
2019-01-20 21:38:54 +0800

    imgdiff: generate new.png if needed https://github.com/xhebox/sbutils/commit/ebb1c7f1b9fe03cd880e171054886649e76bca78 did not solved the issue.
    `C=D;D=E` will also overwrite `C=D`. so we need to generate a new image.

--- imgdiff/README.md
+++ imgdiff/README.md
@@ -1,4 +1,4 @@
-# sbanimation
+# imgdiff
 
 asked by a friend.
 
@@ -8,3 +8,5 @@ this program will read a seriese of images(at least two), and for all images exc
 ./sbanimation 1.png 2.png
 #2: ?replace;xxxxxx=xxxxxx....
 ```
+
+and will output new.png if you can not convert 1.png to other images without modifications. in such case, it will always generate a directive for 1.png.
--- imgdiff/main.go
+++ imgdiff/main.go
@@ -36,13 +36,26 @@ func main() {
 	}
 
 	img := images[0]
-	size := img.Bounds().Size()
+	imgrect := img.Bounds()
+	size := imgrect.Size()
 	for k := range images {
 		sz := images[k].Bounds().Size()
 		if sz.X != size.X || sz.Y != size.Y {
 			log.Fatalf("%d-th image is not of the same size as the first image\n", k)
 		}
 	}
+
+	newimg := image.NewRGBA(imgrect)
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			newimg.Set(x, y, img.At(x, y))
+		}
+	}
+
+	k1 := 0
+	k2 := 0
+	k3 := 0
+	k4 := 0
 
 	for k, l := 1, len(images); k < l; k++ {
 		str := map[string]string{}
@@ -60,17 +73,67 @@ func main() {
 					if v, ok := str[src]; !ok {
 						str[src] = dst
 					} else if v != dst {
-						str[v] = dst
+						k4++
+						if k4 == 256 {
+							k4 = 0
+							k3++
+						}
+						if k3 == 256 {
+							k3 = 0
+							k2++
+						}
+						if k2 == 256 {
+							k2 = 0
+							k1++
+						}
+						if k1 == 256 {
+							log.Fatalf("overflowed, can not transform %d-th image even with new images\n", k+1)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for k, l := 0, len(images); k < l; k++ {
+		str := map[string]string{}
+		cur := images[k]
+
+		for x := 0; x < size.X; x++ {
+			for y := 0; y < size.Y; y++ {
+				cr, cg, cb, ca := cur.At(x, y).RGBA()
+				pr, pg, pb, pa := newimg.At(x, y).RGBA()
+
+				if cr != pr || cg != pg || cb != pb || ca != pa {
+					src := fmt.Sprintf("%02X%02X%02X%02X", uint8(pr), uint8(pg), uint8(pb), uint8(pa))
+					dst := fmt.Sprintf("%02X%02X%02X%02X", uint8(cr), uint8(cg), uint8(cb), uint8(ca))
+
+					if _, ok := str[src]; !ok {
+						str[src] = dst
 					}
 				}
 			}
 		}
 
-		out := &strings.Builder{}
-		fmt.Fprintf(out, "#%d: ?replace", k+1)
-		for k, v := range str {
-			fmt.Fprintf(out, ";%s=%s", k, v)
+		if len(str) != 0 {
+			out := &strings.Builder{}
+			fmt.Fprintf(out, "#%d: ?replace", k+1)
+			for k, v := range str {
+				fmt.Fprintf(out, ";%s=%s", k, v)
+			}
+			fmt.Println(out)
 		}
-		fmt.Println(out)
+	}
+
+	if k1+k2+k3+k4 != 0 {
+		f, e := os.OpenFile("new.png", os.O_RDWR|os.O_CREATE, 0755)
+		if e != nil {
+			log.Fatal(e)
+		}
+		defer f.Close()
+
+		if e := png.Encode(f, newimg); e != nil {
+			log.Fatal(e)
+		}
 	}
 }
